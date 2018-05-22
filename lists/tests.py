d7f20cd1bce617322cae74e79b6a650ef4dfeaaa
wzg <592485607@qq.com>
2018-05-22 20:05:47 +0800

    test_home_page

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_view(self):
+        found = resolve('/')
+        # 检查解析网站根路径"/"时,是否能找到名为home_page的函数
+        self.assertEqual(found.func,home_page)
+
+# class SmokeTest(TestCase):
+    # def test_bad_maths(self):
+    #     self.assertEqual(1+1,3)
